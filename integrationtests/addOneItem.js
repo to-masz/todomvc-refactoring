f5c3eaf20e36da456c4eaf010faeff4c8122300d
Martin Ruprecht <martin.ruprecht@mayflower.de>
2015-05-15 16:27:40 +0200

    Added a new integration test for a new generated todo item and modified the package.json file to execute both tests.

---
+++ integrationtests/addOneItem.js
@@ -0,0 +1,41 @@
+'use strict';
+/* global kommando  require __dirname */
+
+var assertTrimmedString = require('./assertTrimmedString');
+
+var path = require('path');
+var fs = require('fs');
+
+var url = 'http://localhost/todomvc-refactoring/index.html';
+var browser = kommando.browser;
+
+browser.manage().window().setSize(800, 600);
+
+describe('add one item', function () {
+
+    describe('should render correct', function () {
+    });
+});
+it('when one item was added', function (done) {
+    browser.get(url)
+        .then(function () {
+            return browser.findElement(kommando.webdriver.By.id('new-todo')).click();
+        })
+        .then(function () {
+            return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys('hallo');
+        })
+        .then(function () {
+            return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys(kommando.webdriver.Key.ENTER);
+        })
+        .then(function () {
+            return browser.findElement(kommando.webdriver.By.tagName('body')).getInnerHtml();
+        })
+        .then(function (html) {
+            var reactAttrs = / data-react[-\w]+="[^"]+"/g;
+
+            //fs.writeFileSync(path.join(__dirname, 'golden-master-addOneItem.html'), html.replace(reactAttrs, ''));
+            var expected = fs.readFileSync(path.join(__dirname, 'golden-master-addOneItem.html'), 'utf8');
+            assertTrimmedString(html.replace(reactAttrs, ''), expected);
+        })
+        .then(done, done);
+});
