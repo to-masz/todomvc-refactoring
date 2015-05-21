273c517f865b633c1090622eb9d8294d4da6d614
Wolfram Kriesing <wk@uxebu.com>
2015-05-21 22:10:00 +0200

    Make one test more readable.

--- integrationtests/filterCompletedItems.js
+++ integrationtests/filterCompletedItems.js
@@ -12,54 +12,66 @@ var browser = kommando.browser;
 
 browser.manage().window().setSize(800, 600);
 
+function newPage() {
+  return browser
+    .get(url)
+    .then(function () {
+        return browser.executeScript('localStorage.clear();');
+    })
+    .then(function () {
+      return browser.get(url);
+    });
+}
+function addTodo(promise, text) {
+  return promise.then(function () {
+      return browser.findElement(kommando.webdriver.By.id('new-todo')).click();
+  })
+  .then(function () {
+      return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys('hallo');
+  })
+  .then(function () {
+      return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys(kommando.webdriver.Key.ENTER);
+  });
+}
+
+function setFirstItemToCompleted(promise) {
+  // set first item to completed
+  return promise.then(function () {
+      return browser.findElement(kommando.webdriver.By.css('#todo-list li input[type=checkbox]')).click();
+  });
+}
+
+function filterByCompleted(promise) {
+  // click the completed filter button
+  return promise.then(function () {
+      return browser.findElement(kommando.webdriver.By.linkText('Completed')).click();
+  });
+}
+
+function assertEqual(promise, goldenMasterFileName) {
+  return promise
+    .then(function () {
+        return browser.findElement(kommando.webdriver.By.tagName('body')).getInnerHtml();
+    })
+    .then(function (html) {
+        var reactAttrs = / data-react[-\w]+="[^"]+"/g;
+        //fs.writeFileSync(path.join(__dirname, goldenMasterFileName), html.replace(reactAttrs, ''));
+        var expected = fs.readFileSync(path.join(__dirname, goldenMasterFileName), 'utf8');
+        assertTrimmedString(html.replace(reactAttrs, ''), expected);
+    });
+}
+
 describe('filter all completed items', function () {
 
   it('shows only the completed item', function(done) {
     // add 2 items
-    browser
-      .get(url)
-      .then(function () {
-          return browser.executeScript('localStorage.clear();');
-      })
-      .then(function () {
-        return browser.get(url);
-      })
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.id('new-todo')).click();
-      })
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys('hallo');
-      })
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys(kommando.webdriver.Key.ENTER);
-      })
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.id('new-todo')).click();
-      })
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys('hallo 1');
-      })
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.id('new-todo')).sendKeys(kommando.webdriver.Key.ENTER);
-      })
-      // set first item to completed
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.css('#todo-list li input[type=checkbox]')).click();
-      })
-      // click the completed filter button
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.linkText('Completed')).click();
-      })
-      .then(function () {
-          return browser.findElement(kommando.webdriver.By.tagName('body')).getInnerHtml();
-      })
-      .then(function (html) {
-          var reactAttrs = / data-react[-\w]+="[^"]+"/g;
-          //fs.writeFileSync(path.join(__dirname, 'golden-master-filterCompletedItems.html'), html.replace(reactAttrs, ''));
-          var expected = fs.readFileSync(path.join(__dirname, 'golden-master-filterCompletedItems.html'), 'utf8');
-          assertTrimmedString(html.replace(reactAttrs, ''), expected);
-      })
-      .then(done, done);
+    var promise = newPage();
+    promise = addTodo(promise, 'hallo');
+    promise = addTodo(promise, 'hallo 1');
+    promise = setFirstItemToCompleted(promise);
+    promise = filterByCompleted(promise);
+    promise = assertEqual(promise, 'golden-master-filterCompletedItems.html');
+    promise.then(done, done);
   });
 
   it('updates the URL', function(done) {
