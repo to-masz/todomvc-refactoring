eb51206aa3353499f5f89d80140cdae0921ae755
Wolfram Kriesing <wk@uxebu.com>
2015-05-22 10:45:51 +0200

    Extract filter into own file, fails due to missing proper module system. => lets use browserify

---
+++ js/filter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function Filter(filters) {
+  this._filters = filters;
+  this._current = 0;
+}
+
+Filter.prototype = {
+  applyOn: function(todos) {
+    return this._filters[this._current].criteria ? todos.filter(this._filters[this._current].criteria) : todos;
+  },
+
+  getViewValues: function() {
+    var currentIndex = this._current;
+    return this._filters.map(function(by, idx) {
+      return {url: '#' + by.url, linkText: by.linkText, selected: idx === currentIndex};
+    });
+  }
+};
--- js/main.jsx
+++ js/main.jsx
@@ -1,23 +1,6 @@
 'use strict';
 /* global React Router */
 var app = app || {};
-function Filter(filters) {
-  this._filters = filters;
-  this._current = 0;
-}
-
-Filter.prototype = {
-  applyOn: function(todos) {
-    return this._filters[this._current].criteria ? todos.filter(this._filters[this._current].criteria) : todos;
-  },
-
-  getViewValues: function() {
-    var currentIndex = this._current;
-    return this._filters.map(function(by, idx) {
-      return {url: '#' + by.url, linkText: by.linkText, selected: idx === currentIndex};
-    });
-  }
-};
 
 app.TodoItem = React.createClass({
   sbmt: function() {
