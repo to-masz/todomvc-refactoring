cdfc34b960356dfef424bfd3f3e0306adc7d82dc
Wolfram Kriesing <wk@uxebu.com>
2015-05-22 10:42:55 +0200

    Recreate the golden masters ONLY using the todoapp-HTML-node.

--- integrationtests/_controls.js
+++ integrationtests/_controls.js
@@ -47,7 +47,7 @@ module.exports = {
   assertInnerHtml: function(promise, goldenMasterFileName) {
     return promise
       .then(function() {
-        return browser.findElement(kommando.webdriver.By.tagName('body')).getInnerHtml();
+        return browser.findElement(kommando.webdriver.By.css('#todoapp')).getInnerHtml();
       })
       .then(function(html) {
         var reactAttrs = / data-react[-\w]+="[^"]+"/g;
