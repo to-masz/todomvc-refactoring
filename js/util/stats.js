4ca659770511714307217d1043dab4f05159ad6b
Wolfram Kriesing <wk@uxebu.com>
2015-05-22 11:33:27 +0200

    Simple refactoring.

--- js/util/stats.js
+++ js/util/stats.js
@@ -2,11 +2,15 @@
 /* global module */
 
 function stats(todos) {
-  var activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
+  var activeCount = todos.filter(isActive).length;
   return {
     activeCount: activeCount,
     completedCount: todos.length - activeCount
   };
+}
+
+function isActive(todo) {
+  return !todo.completed;
 }
 
 module.exports = stats;
