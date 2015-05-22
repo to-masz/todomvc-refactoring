544d338418afe6956f09d14f07ddf3b6e9696c6f
Wolfram Kriesing <wk@uxebu.com>
2015-05-22 10:57:43 +0200

    Make the modules node-style modules.

--- js/filter.js
+++ js/filter.js
@@ -17,3 +17,5 @@ Filter.prototype = {
     });
   }
 };
+
+module.exports = Filter;
--- js/todo-model.js
+++ js/todo-model.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = app || {};
+
 function more() {
   var newObj = {};
   for (var i = 0; i < arguments.length; i++) {
@@ -18,22 +18,22 @@ function more() {
 // may not even be worth separating this logic
 // out, but we do this to demonstrate one way to
 // separate out parts of your application.
-app.TodoModel = function(key) {
+function TodoModel(key) {
   this.key = key;
   this.todos = (localStorage.getItem(key) && JSON.parse(localStorage.getItem(key))) || [];
   this.onChanges = [];
-};
+}
 
-app.TodoModel.prototype.subscribe = function(onChange) {
+TodoModel.prototype.subscribe = function(onChange) {
   this.onChanges.push(onChange);
 };
 
-app.TodoModel.prototype.inform = function() {
+TodoModel.prototype.inform = function() {
   localStorage.setItem(this.key, JSON.stringify(this.todos));
   this.onChanges.forEach(function(cb) { cb(); });
 };
 
-app.TodoModel.prototype.toggleAll = function(checked) {
+TodoModel.prototype.toggleAll = function(checked) {
   // Note: it's usually better to use immutable data structures since they're
   // easier to reason about and React works very well with them. That's why
   // we use map() and filter() everywhere instead of mutating the array or
@@ -45,7 +45,7 @@ app.TodoModel.prototype.toggleAll = function(checked) {
   this.inform();
 };
 
-app.TodoModel.prototype.toggle = function(todoToToggle) {
+TodoModel.prototype.toggle = function(todoToToggle) {
   this.todos = this.todos.map(function(todo) {
     return todo !== todoToToggle ?
       todo :
@@ -55,7 +55,7 @@ app.TodoModel.prototype.toggle = function(todoToToggle) {
   this.inform();
 };
 
-app.TodoModel.prototype.destroy = function(todo) {
+TodoModel.prototype.destroy = function(todo) {
   this.todos = this.todos.filter(function(candidate) {
     return candidate !== todo;
   });
@@ -63,7 +63,7 @@ app.TodoModel.prototype.destroy = function(todo) {
   this.inform();
 };
 
-app.TodoModel.prototype.save = function(todoToSave, text) {
+TodoModel.prototype.save = function(todoToSave, text) {
   this.todos = this.todos.map(function(todo) {
     return todo !== todoToSave ? todo : more({}, todo, {title: text});
   });
@@ -71,10 +71,12 @@ app.TodoModel.prototype.save = function(todoToSave, text) {
   this.inform();
 };
 
-app.TodoModel.prototype.clearCompleted = function() {
+TodoModel.prototype.clearCompleted = function() {
   this.todos = this.todos.filter(function(todo) {
     return !todo.completed;
   });
 
   this.inform();
 };
+
+module.exports = TodoModel;
