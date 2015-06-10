52f75ea74c12d3cbd59fd7a5900c30eced38baed
Wolfram Kriesing <wk@uxebu.com>
2015-06-10 14:41:03 +0200

    Implementing UrlStorage.

--- test/urlstorage-spec.js
+++ test/urlstorage-spec.js
@@ -6,18 +6,45 @@ var storageInterfaceChecks = require('./_storage-interface-checks');
 
 describe('URL storage', function() {
 
-  var storage;
-  beforeEach(function() {
-    storage = new UrlStorage();
+  it('verify interface', function() {
+    var storage = new UrlStorage();
+    storageInterfaceChecks(storage, 'UrlStorage');
   });
 
-  it('verify interface', function() {
-    storageInterfaceChecks(storage, 'UrlStorage');
+  describe('`get()`', function() {
+    it('properly decodes the data from the URL query param', function() {
+      var todos = [{id: 1, title: 'title'}];
+      var urlQueryContent = {get: function() { return JSON.stringify(todos); }};
+      var storage = new UrlStorage(urlQueryContent);
+
+      assert.deepEqual(storage.get(), todos);
+    });
+    describe('with invalid input', function() {
+      it('doesn`t bail out on non-JSON content', function() {
+        var urlQueryContent = {get: function() { return 'invalid JSON data'; }};
+        var storage = new UrlStorage(urlQueryContent);
+
+        assert.doesNotThrow(function() { storage.get(); });
+      });
+      it('returns an empty todo list', function() {
+        var urlQueryContent = {get: function() { return 'invalid JSON data'; }};
+        var storage = new UrlStorage(urlQueryContent);
+
+        assert.deepEqual(storage.get(), []);
+      });
+    });
   });
 
 });
 
-function UrlStorage() {
-  this.get = function() {};
+function UrlStorage(urlQueryContent) {
+  this.get = function() {
+    try {
+      return JSON.parse(urlQueryContent.get());
+    } catch (e) {
+      var noData = [];
+      return noData;
+    }
+  };
   this.set = function() {};
 }
